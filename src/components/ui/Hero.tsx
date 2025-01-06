451d27d909f69f7809098a50f62fd684c617c84a
Fede Herrera <herrera3299@gmail.com>
2025-01-06 10:59:09 -0300

    actualizacion pagina index, instalacion fontsource roboto

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -2,22 +2,19 @@ import ImgHero from '../../assets/img/hero.png';
 
 export const Hero = () => {
   return (
-    <div className='p-4 flex flex-col items-center justify-center' id="inicio">
-    
-    <div className='flex flex-col justify-center items-center bg-indigo-400 md:flex-row md:justify-between md:items-start rounded-xl '>
-        <div className='md:w-2/5 p-6 my-3 leading-snug flex flex-col'>
+    <div className='flex flex-col items-center justify-center' id="inicio">
+      <div className='flex flex-col justify-center items-center bg-indigo-400 lg:flex-row lg:justify-between lg:items-start rounded-xl '>
+        <div className='lg:w-2/5 p-6 my-3 leading-snug flex flex-col'>
         <h1 className='text-3xl font-bold text-slate-50 md:text-5xl'> <span className='text-zinc-900'>En VetApp</span> , agenda tu turno y dale a tu mascota la atención que necesita.</h1>
         <p className='mt-5 text-slate-50 md:text-lg md:mt-10 lg:mt-16'>Profesionales en cuidado animal con servicios personalizados.
         Atendemos emergencias, consultas, vacunación y mucho más
         </p>
-        <button className='hidden md:block p-2 max-w-[200px] mt-10 bg-zinc-900 rounded-full text-slate-50 hover:bg-zinc-800 transition-colors lg:mt-16'>Reservar Turno</button>
+        <button className='hidden lg:block p-2 max-w-[200px] mt-10 bg-zinc-900 rounded-full text-slate-50 hover:bg-zinc-800 transition-colors lg:mt-16'>Reservar Turno</button>
         </div>
         
-        <img src={ImgHero} alt='img-hero' className='h-60  md:w-3/5 md:h-screen object-contain xl:h-[80vh]'/>
-    </div>
-    
-    <button className=' block md:hidden p-2 w-full max-w-sm mx auto mt-2 bg-zinc-900 rounded-full text-slate-50 hover:bg-zinc-800 transition-colors'>Reservar Turno</button>
-    
+        <img src={ImgHero} alt='img-hero' className='h-60  lg:w-3/5 lg:h-screen object-contain xl:h-[80vh]'/>
+      </div>
+      <button className=' block lg:hidden p-2 w-full max-w-sm mx auto mt-2 bg-zinc-900 rounded-full text-slate-50 hover:bg-zinc-800 transition-colors'>Reservar Turno</button>
     </div>
   )
 }
--- src/layouts/sections/SectionStep.tsx
+++ src/layouts/sections/SectionStep.tsx
@@ -1,33 +1,32 @@
 export const SectionStep = () => {
   return (
-    <section className="flex flex-col p-4 justify-center md:flex-row gap-20" id="step">
-        <h2 className="text-2xl font-normal text-center mb-5 lg:text-3xl">¿Como usar nuestro servicio?</h2>
-        
+    <section className="flex flex-col  justify-center md:flex-row md:gap-20 md:mt-28 xl:mt-40" id="step">
+        <h2 className="my-20 md:my-0 text-2xl font-normal text-center  lg:text-3xl xl:text-4xl">¿Como usar nuestro servicio?</h2>
         <div className="w-full md:w-1/2">
         
-        <div className="mt-8 md:mt-0">
+        <div className="">
             <div className="mb-4">
-                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl">01</p>
-                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl">Completa el formuario</h3>
+                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl xl:text-2xl">01</p>
+                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl xl:text-2xl">Completa el formuario</h3>
             </div>
-            <p className="text-md  text-slate-600 lg:text-lg">Ingresa en la seccion ingresa, completa los datos del formulario</p>
+            <p className="text-md  text-slate-600 lg:text-lg xl:text-xl">Ingresa en la seccion ingresa, completa los datos del formulario</p>
         </div>
 
         <div className="mt-8">
             <div className="mb-4">
-                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl">02</p>
-                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl">Valida tu cuenta</h3>
+                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl xl:text-2xl">02</p>
+                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl xl:text-2xl">Valida tu cuenta</h3>
             </div>
-            <p className="text-md  text-slate-600 lg:text-lg">Ingresa a tu casilla de correo  donde te enviaremos un codigo para confirmar tu cuenta</p>
+            <p className="text-md  text-slate-600 lg:text-lg xl:text-xl">Ingresa a tu casilla de correo  donde te enviaremos un codigo para confirmar tu cuenta</p>
         </div>
 
 
         <div className="mt-8">
             <div className="mb-4">
-                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl" >03</p>
-                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl">Agenda tus turnos</h3>
+                <p className="text-lg font-medium text-indigo-500 inline lg:text-xl xl:text-2xl" >03</p>
+                <h3 className="text-lg font-medium  inline ml-3 lg:text-xl xl:text-2xl">Agenda tus turnos</h3>
             </div>
-            <p className="text-md  text-slate-600 lg:text-lg ">Si todos los pasos estan correctos, ya puedes agendar turnos con nuestros profesionales</p>
+            <p className="text-md  text-slate-600 lg:text-lg  xl:text-xl">Si todos los pasos estan correctos, ya puedes agendar turnos con nuestros profesionales</p>
         </div>
         
         </div>
