288e7600ed07b35b0ecfff6925cbac7ec7b5d808
Fede Herrera <herrera3299@gmail.com>
2025-02-25 09:51:02 -0300

    agregando componente spinner

---
+++ src/components/ui/Spinner.tsx
@@ -0,0 +1,12 @@
+import "../../assets/css/spinner.css";
+export const Spinner = () => {
+  return (
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  );
+};
